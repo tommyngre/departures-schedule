dba8f73b800e7a5a5fbd75dfaaa84bc2cc3ed945
unknown <tommyn.gre@gmail.com>
2018-04-05 07:39:55 -0400

    good start on things

---
+++ assets/js/app.js
@@ -0,0 +1,99 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC25-WUlZIYukycskRciNzYbNkz0In1yRo",
+  authDomain: "testing-c7ec4.firebaseapp.com",
+  databaseURL: "https://testing-c7ec4.firebaseio.com",
+  projectId: "testing-c7ec4",
+  storageBucket: "testing-c7ec4.appspot.com",
+  messagingSenderId: "498816457214"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+// initial Values
+var name = "";
+var destination = "";
+var firstTrainAt = "";
+var frequency = "";
+
+function validateTime(time) {
+  time = moment(time, "HH:mm").format("HH:mm");
+  console.log(time, "validateTime()");
+  return time;
+}
+
+$("#add-user").on("click", function (event) {
+  event.preventDefault();
+
+  // values from inputs
+  name = $("#nameInput").val();
+  destination = $("#destinationInput").val();
+  firstTrainAt = validateTime($("#firstAtInput").val());
+  frequency = $("#frequencyInput").val();
+
+  database.ref().push({
+    name: name,
+    destination: destination,
+    firstTrainAt: firstTrainAt,
+    frequency: frequency
+  });
+
+});
+
+// Firebase watcher + initial loader + order/limit HINT: .on("child_added"
+
+//from 02-recent-user-with-push
+//database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+
+function getNextTrain(firstTrainAt, frequency) {
+  firstTrainAt = moment(firstTrainAt, "HH:mm");
+
+  let currentTime = moment();
+
+  //just so i don't need to enter it while testing
+  frequency = 15;
+
+  let diff = firstTrainAt.diff(currentTime, "minutes");
+
+  do {
+    diff = firstTrainAt.diff(currentTime, "minutes");
+    console.log(diff);
+    firstTrainAt.add(frequency, 'minutes');
+  } while (diff < 0)
+
+  console.log(diff, " after do/while");
+
+  return diff
+}
+
+database.ref().on("child_added", function (snapshot) {
+
+  // store snapshot
+  var s = snapshot.val();
+
+  let nextTrainIn = getNextTrain(s.firstTrainAt, s.frequency); // moment(sv.start).fromNow();
+  //console.log(nextTrainIn, "nextTrainIn");
+
+
+  // Change the HTML to reflect
+  let html =
+    `
+  <td>${s.name}</td>
+  <td>${s.destination}</td>
+  <td>${s.firstTrainAt}</td>
+  <td>${s.frequency}</td>
+  <td>${nextTrainIn} minutes</td>
+
+  `
+
+  let newLine = $("<tr>").html(html);
+  $("#appendLinesTo").append(newLine);
+
+
+  // Handle the errors
+}, function (errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
